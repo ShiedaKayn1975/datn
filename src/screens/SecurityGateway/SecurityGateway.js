41d2ffe2c946e3c6b1bd32f7c72c1843e918ed67
littleBoy1945 <allmid66@gmail.com>
2022-03-25 23:48:24 +0700

    date picker

--- src/screens/SecurityGateway/SecurityGateway.js
+++ src/screens/SecurityGateway/SecurityGateway.js
@@ -34,7 +34,7 @@ export const SecurityGateway = (props) => {
       <SecurityWrapper>
         <Grid container direction="column" justifyContent="flex-end" sx={{ minHeight: '100vh' }}>
           <Grid item xs={12}>
-            <Grid container justifyContent="center" alignItems="center" sx={{ minHeight: 'calc(100vh - 68px)' }}>
+            <Grid container justifyContent="center" alignItems="center" sx={{ minHeight: '100vh' }}>
               <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
                 <MainCardWrapper>
                   <Grid container spacing={2} alignItems="center" justifyContent="center">
