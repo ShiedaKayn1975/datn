c3c669359b5ff6fdcdc15ed58cf945794622d074
littleBoy1945 <allmid66@gmail.com>
2022-03-27 22:14:57 +0700

    update

--- src/screens/SecurityGateway/SecurityGateway.js
+++ src/screens/SecurityGateway/SecurityGateway.js
@@ -11,6 +11,7 @@ export const SecurityGateway = (props) => {
   const theme = useTheme()
   const matchDownSM = useMediaQuery(theme.breakpoints.down('md'));
   const securitySteps = useSelector(state => state.securityGateway)
+  const [data, setData] = useState(null)
   let [currentStep, setCurrentStep] = useState(null)
   let [step, setStep] = useState(null)
 
@@ -20,6 +21,16 @@ export const SecurityGateway = (props) => {
       setStep(securitySteps.steps[securitySteps.currentStep])
     }
   }, [securitySteps])
+
+  const onSetData = (key, value) => {
+    let currentData = data || {}
+    currentData[key] = value
+    setData(currentData)
+  }
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
 
   const onIgnoreStep = () => {
 
@@ -36,87 +47,82 @@ export const SecurityGateway = (props) => {
           <Grid item xs={12}>
             <Grid container justifyContent="center" alignItems="center" sx={{ minHeight: '100vh' }}>
               <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
-                <MainCardWrapper>
-                  <Grid container spacing={2} alignItems="center" justifyContent="center">
-                    <Grid item xs={12}>
-                      <Grid
-                        container
-                        direction={matchDownSM ? 'column-reverse' : 'row'}
-                        alignItems="center"
-                        justifyContent="center"
-                      >
-                        <Grid item sx={{ mb: 3 }}>
-                          <Typography
-                            color={theme.palette.success.dark}
-                            gutterBottom
-                            variant={matchDownSM ? 'h4' : 'h3'}
-                          >
-                            {step?.title}
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                          <Divider />
-                        </Grid>
-                        <Grid item xs={12} sx={{ mt: 2 }}>
-                          <Stack alignItems="center" justifyContent="center" spacing={1}>
-                            {/* <Typography
-                              color={theme.palette.secondary.main}
+                {
+                  step &&
+                  <MainCardWrapper>
+                    <Grid container spacing={2} alignItems="center" justifyContent="center">
+                      <Grid item xs={12}>
+                        <Grid
+                          container
+                          direction={matchDownSM ? 'column-reverse' : 'row'}
+                          alignItems="center"
+                          justifyContent="center"
+                        >
+                          <Grid item sx={{ mb: 3 }}>
+                            <Typography
+                              color={theme.palette.success.dark}
                               gutterBottom
-                              variant={matchDownSM ? 'h3' : 'h2'}   
+                              variant={matchDownSM ? 'h4' : 'h3'}
                             >
-                              Enter your birthday
-                            </Typography> */}
-                            <SecureRenderer
-                              onIgnoreStep={onIgnoreStep}
-                              onSubmitStep={onSubmitStep}
-                              currentStep={currentStep}
-                              step={step}
-                            />
-                          </Stack>
+                              {step?.title}
+                            </Typography>
+                          </Grid>
+                          <Grid item xs={12}>
+                            <Divider />
+                          </Grid>
+                          <Grid item xs={12} sx={{ mt: 2 }}>
+                            <Stack alignItems="center" justifyContent="center" spacing={1}>
+                              <SecureRenderer
+                                onSetData={onSetData}
+                                currentStep={currentStep}
+                                step={step}
+                              />
+                            </Stack>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Divider />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Grid container>
+                          <Grid item container direction="column" alignItems="start" xs={6}>
+                            <Typography
+                              variant="subtitle1"
+                              sx={{
+                                textDecoration: 'underline',
+                                cursor: 'pointer',
+                                float: 'right',
+                                color: theme.palette.secondary.main,
+                                '&:hover': {
+                                  color: theme.palette.secondary.dark
+                                }
+                              }}
+                            >
+                              Ignore
+                            </Typography>
+                          </Grid>
+                          <Grid item container direction="column" alignItems="end" xs={6}>
+                            <Typography
+                              variant="subtitle1"
+                              sx={{
+                                textDecoration: 'underline',
+                                cursor: 'pointer',
+                                float: 'right',
+                                color: theme.palette.success.main,
+                                '&:hover': {
+                                  color: theme.palette.success.dark
+                                }
+                              }}
+                            >
+                              Next step
+                            </Typography>
+                          </Grid>
                         </Grid>
                       </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                      <Divider />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <Grid container>
-                        <Grid item container direction="column" alignItems="start" xs={6}>
-                          <Typography
-                            variant="subtitle1"
-                            sx={{
-                              textDecoration: 'underline',
-                              cursor: 'pointer',
-                              float: 'right',
-                              color: theme.palette.secondary.main,
-                              '&:hover': {
-                                color: theme.palette.secondary.dark
-                              }
-                            }}
-                          >
-                            Ignore
-                          </Typography>
-                        </Grid>
-                        <Grid item container direction="column" alignItems="end" xs={6}>
-                          <Typography
-                            variant="subtitle1"
-                            sx={{
-                              textDecoration: 'underline',
-                              cursor: 'pointer',
-                              float: 'right',
-                              color: theme.palette.success.main,
-                              '&:hover': {
-                                color: theme.palette.success.dark
-                              }
-                            }}
-                          >
-                            Next step
-                          </Typography>
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </MainCardWrapper>
+                  </MainCardWrapper>
+                }
               </Grid>
             </Grid>
           </Grid>
